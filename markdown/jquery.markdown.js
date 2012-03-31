bff7ce4940c7c32af6aa65419235665d64227a2d
Andrew Preston <andrew.preston@gmail.com>
2012-03-31 14:46:32 +1300

    Initial code for the plugin itself.

---
+++ markdown/jquery.markdown.js
@@ -0,0 +1,111 @@
+/*jshint smarttabs:true*/
+
+/*
+ * jquery.markdown.js
+ *
+ * Author: Andrew Preston | preston.co.nz
+ *
+ * Extends the stackoverflow markdown editor [1] to work as a jQuery
+ * plugin [2].
+ *
+ * [1] http://code.google.com/p/pagedown/
+ * [2] http://docs.jquery.com/Plugins/Authoring
+ */
+
+;(function($) {
+    'use strict';
+
+    var methods = {
+	init : function(options) {
+	    return this.each(function(index) {
+		var elem = $(this); // equivalent: this.eq(index);
+
+		var defaults = {
+		    'buttonClass'  : 'markdown-buttons',
+		    'textareaClass': 'markdown-editor',
+		    'previewClass' : 'markdown-preview'
+		};
+
+		var settings = $.extend({}, defaults, options);
+
+		/*
+		 * Initialize the markdown editor if one is not
+		 * already attached to elem:
+		 *   1) find (or create) textarea, button, and preview children
+		 *   2) instantiate a new editor
+		 *   3) attach to elem as .data()
+		 */
+		if (!elem.data('markdown')) {
+
+		    // 1) Find/create required elements
+		    var textarea = elem.find( '.'+settings.textareaClass );
+		    var buttons  = elem.find( '.'+settings.buttonClass );
+		    var preview  = elem.find( '.'+settings.previewClass );
+
+		    if (!textarea.length) {
+			textarea = $('<textarea/>', {
+			    class: settings.textareaClass,
+			    id   : settings.textareaClass + index
+			});
+			elem.append(textarea);
+		    }
+
+		    if (!buttons.length) {
+			buttons  = $('<div/>', {
+			    class: settings.buttonClass,
+			    id   : settings.buttonClass + index
+			}).insertBefore(textarea);
+		    }
+
+		    if (!preview.length) {
+			preview  = $('<div/>', {
+			    class: settings.previewClass,
+			    id   : settings.previewClass + index
+			}).insertAfter(textarea);
+		    }
+
+		    // 2) Create the editor!
+		    var converter = Markdown.getSanitizingConverter();
+		    var editor    = new Markdown.Editor(
+			converter,
+			buttons.attr('id'),
+			textarea.attr('id'),
+			preview.attr('id')
+		    );
+		    editor.run();
+
+		    // 3) Attach editor to the DOM
+		    elem.data('markdown', {
+			target   : elem,
+			settings : settings,
+			editor   : editor
+		    });
+		}
+	    }); },
+
+	refreshPreview : function() {
+	    return this.each(function() {
+		var elem   = $(this);
+		var editor = elem.data('markdown').editor;
+		editor.refreshPreview();
+	    }); }
+    };
+
+    $.fn.markdown = function(method) {
+	if (methods[method]) {
+	    return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+	} else if (typeof method === 'object' || !method) {
+	    return methods.init.apply(this, arguments);
+	} else {
+	    $.error('Method ' + method + ' is not defined for jQuery.markdown');
+	}
+    };
+
+})(jQuery);
+
+// $(document).ready(function() {
+//     /*
+//      * Init an editor on every .markdown-input in the DOM
+//      */
+//     $( ".markdown-input" ).markdown();
+// });
